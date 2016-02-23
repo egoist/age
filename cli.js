17b76b8eafdb5489c49470d13f490002099069df
EGOIST <0x142857@gmail.com>
2016-02-23 12:17:48 +0800

    improve arg parsing

--- cli.js
+++ cli.js
@@ -1,12 +1,32 @@
 #!/usr/bin/env node
 require('colorful').toxic()
-const argv = require('minimist')(process.argv.slice(2), { '--': true })
+const argv = require('minimist')(process.argv.slice(2), {
+	'--': true,
+	alias: {
+		v: 'version',
+		h: 'help',
+		i: 'init'
+	}
+})
 const age = require('./')
 const pkg = require('./package')
 
-if (argv.v || argv.version) {
-  console.log(pkg.name, '~', pkg.version.cyan)
-  return
+if (argv.v) {
+	console.log(pkg.name, '~', pkg.version.cyan)
+	process.exit()
+}
+
+if (argv.h) {
+	console.log([
+		'',
+		'  Usage:',
+		'    -i/--init:       Interactively set birthday',
+		'',
+		'    -v/--version:    Print version',
+		'    -h/--help:       Print help',
+		''
+	].join('\n'))
+	process.exit()
 }
 
 age(argv)
